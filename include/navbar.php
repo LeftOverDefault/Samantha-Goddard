e90e9c18aef05f7ba6231dc97b599853a98bc68b
Michael Goddard <michaelgoddard@outlook.com>
2023-12-08 22:58:00 +0000

    added all required pages

---
+++ include/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar">
+    <div class="nav-item">
+        <a href="#">
+            <h1 class="logo">Samantha Goddard</h1>
+        </a>
+    </div>
+    <div class="nav-item">
+        <div class="menu">
+            <div class="menu-wrapper">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="dropdown-container">
+                <ul class="dropdown">
+                    <li><a href="./">Home</a></li>
+                    <li><a href="./whatisrtt">What Is RTT</a></li>
+                    <li><a href="./workingwithme">Working With Me</a></li>
+                    <li><a href="./audios">Audios</a></li>
+                    <li><a href="./contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("./include/header.php"); ?>
+</head>
+<body>
+
+    <?php include_once("./include/navbar.php"); ?>
+
+    <?php
+        if (isset($_GET["p"])) {
+            $current_page = $_GET["p"];
+        } else {
+            $current_page = "";
+        }
+
+        switch ($current_page) {
+            case "workingwithme":
+                include_once("./pages/workingwithme.php");
+                break;
+            case "audios":
+                include_once("./pages/audios.php");
+                break;
+            case "contact":
+                include_once("./pages/contact.php");
+                break;
+            case "whatisrtt":
+                include_once("./pages/whatisrtt.php");
+                break;
+            default:
+                include_once("./pages/home.php");
+                break;
+            }
+            ?>
+
+    <?php include_once("./include/footer.php"); ?>
+
+    <script src="./js/script.js"></script>
+</body>
+</html>
