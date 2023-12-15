202f79fa7b9ec007df366db72c15923596bf8c7e
Michael Goddard <michaelgoddard@outlook.com>
2023-12-15 21:01:29 +0000

    attempted fixing php

---
+++ copy.bat
@@ -0,0 +1,3 @@
+@echo Off
+
+Xcopy /E /I C:\Users\micha\Repositories\samantha-goddard C:\Users\micha\wamp\www\samantha-goddard
--- include/header.php
+++ include/header.php
@@ -1,14 +1,33 @@
+<?php
+if (isset($_GET["p"])) {
+	$current_page = $_GET["p"];
+} else {
+	$current_page = "";
+}
+?>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Samantha Goddard | Rapid Transformational Therapist</title>
+<title>Samantha Goddard | Rapid Transformational Therapist<?php
+switch ($current_page) {
+	case "whatisrtt":
+		echo " | What Is RTT?";
+		break;
+	case "audios":
+		echo " | Hypnotic Audios";
+		break;
+	case "workingwithme":
+		echo " | Working With Me";
+		break;
+	case "contact":
+		echo " | Contact";
+		break;
+	default:
+
+		break;
+} ?></title>
 
 <?php
-	if (isset($_GET["p"])) {
-		$current_page = $_GET["p"];
-	} else {
-		$current_page = "";
-	}
-
 	switch ($current_page) {
 		case "whatisrtt":
 			echo "<meta name='description' content='What Is RTT, Rapid Transformational Therapy, Hypnosis, Fast Acting, Immediate,  Immediate transformational therapy'>";
@@ -35,18 +54,18 @@
 	}
 ?>
 
-<meta property="og:locale" content="en_GB" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="Rapid Transformational Therapy with Samantha Goddard, Hypnotherapy, Sevenoaks, Tunbridge Wells, Tonbridge, Kent, Online" />
-<meta property="og:description" content="Hypnotherapy near me, RTT Therapy, RTT near me, Hypnotherapist, Therapist, Weight loss, Emotional eating issues, Anxiety, Panic attacks, Self sabotage, Limiting beliefs, Low self esteem, Depression, Public speaking, Confidence, Insomnia, Sleep issues" />
-<meta property="og:url" content="https://www.samantha-goddard.com/" />
-<meta property="og:site_name" content="Samantha Goddard | Rapid Transformational Therapy" />
-<meta property="article:modified_time" content="2023-07-01T08:45:38+00:00" />
-<meta property="og:image" content="https://www.samantha-goddard.com/img/main-banner.jpg" />
-<meta property="og:image:width" content="1920" />
-<meta property="og:image:height" content="974" />
-<meta property="og:image:type" content="image/jpeg" />
-<meta name="twitter:card" content="summary_large_image" />
+<meta property="og:locale" content="en_GB"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="Rapid Transformational Therapy with Samantha Goddard, Hypnotherapy, Sevenoaks, Tunbridge Wells, Tonbridge, Kent, Online"/>
+<meta property="og:description" content="Hypnotherapy near me, RTT Therapy, RTT near me, Hypnotherapist, Therapist, Weight loss, Emotional eating issues, Anxiety, Panic attacks, Self sabotage, Limiting beliefs, Low self esteem, Depression, Public speaking, Confidence, Insomnia, Sleep issues"/>
+<meta property="og:url" content="https://www.samantha-goddard.com/"/>
+<meta property="og:site_name" content="Samantha Goddard | Rapid Transformational Therapy"/>
+<meta property="article:modified_time" content="2023-07-01T08:45:38+00:00"/>
+<meta property="og:image" content="https://www.samantha-goddard.com/img/main-banner.jpg"/>
+<meta property="og:image:width" content="1920"/>
+<meta property="og:image:height" content="974"/>
+<meta property="og:image:type" content="image/jpeg"/>
+<meta name="twitter:card" content="summary_large_image"/>
 
 <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico">
 <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
